b5b3abef831051063cad75373ec5fbe135c33a5d
Olena Poroskun <56368022+alenaporoskun@users.noreply.github.com>
2024-02-17 18:29:38 +0200

    console_assistant ver_0.2
    Version with OOP principles.

--- Python_projects/Corgi-s-personal-assistant/console_assistant/setup.py
+++ Python_projects/Corgi-s-personal-assistant/console_assistant/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='console_assistant',
-    version='0.1',
+    name='console_assistant2',
+    version='0.2',
     description="A very useful console assistant for Santa's assistant, Mr. Corgi.",
     url='https://github.com/alenaporoskun/Christmas-developers_Corgi-s-personal-assistant-Group14',
     packages=find_packages(),
